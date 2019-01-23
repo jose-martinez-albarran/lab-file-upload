105b9d7e7f5280d5b7f623b1ec123fde02e8c1de
jose-martinez-albarran <jose.martinez.martz@gmail.com>
2019-01-22 21:54:22 -0600

    Avance PP

--- starter-code/app.js
+++ starter-code/app.js
@@ -72,13 +72,13 @@ passport.use('local-signup', new LocalStrategy(
                 const {
                   username,
                   email,
-                  password
+                  password,
                 } = req.body;
                 const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
                 const newUser = new User({
                   username,
                   email,
-                  password: hashPass
+                  password: hashPass,
                 });
 
                 newUser.save((err) => {
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,6 +1,8 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const User       = require('../models/user')
+const uploadCloud = require('../config/cloudinary')
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
@@ -29,6 +31,17 @@ router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     });
 });
 
+router.post('/profile-upload', uploadCloud.single("photo"), ensureLoggedIn('/login'), (req,res)=>{
+ const User = req.user
+ User.updateOne({username: req.user.username},{photo: req.file.url})
+ .then((user)=>{
+    res.render('profile', {user});
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+});
+  
 router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
